734432c4bb1a2d69f0f64b45536df575d0f7f069
chamaling <lamin.bangura113@gmail.com>
2026-04-01 00:38:03 -0400

    feat: connected base loader to layout

--- app/components/GoogleLoader.tsx
+++ app/components/GoogleLoader.tsx
@@ -1,11 +1,21 @@
+"use client"
 import Script from "next/script"
+import config from "@/lib/config"
 export default function GoogleLoader() {
-  function handleGoogleScriptLoad() {}
+  function handleGoogleScriptLoad() {
+    console.log("Google accounts script loaded successfully.")
+  }
+
+  function handleGoogleScriptError() {
+    console.error("Failed to load the Google accounts script.")
+  }
+
   return (
     <Script
       src="https://accounts.google.com/gsi/client"
       strategy="afterInteractive"
       onLoad={handleGoogleScriptLoad}
+      onError={handleGoogleScriptError}
     />
   )
 }
