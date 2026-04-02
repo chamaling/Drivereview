00c1b0062ba977c79e1d446e661339e1e7221e35
chamaling <lamin.bangura113@gmail.com>
2026-04-02 09:53:46 -0400

    feat: built and styled hero

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import { Button } from "@/components/ui/button"
 import GoogleDriveButton from "./components/GoogleDriveButton"
 export default function Page() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex flex-col h-screen items-center justify-center">
       <header>{/* TODO: add icon */}</header>
-      <main>
-        <h1>Claim your Google Drive storage back</h1>
+      <main className="flex flex-col items-center gap-4">
+        <h1 className="text-5xl font-semibold">Claim your Google Drive storage back</h1>
+        <p className="text-center text-muted-foreground">Drivereview analyzes your Google Drive usage and gives best recommendations for optimizing your storage.</p>
         <GoogleDriveButton />
       </main>
-      <footer>
+      <footer className="mt-auto text-sm text-muted-foreground">
         <p>
           &copy; {new Date().getFullYear()} Lamin Bangura. All rights reserved.
         </p>
