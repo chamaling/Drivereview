595887608a3d55ff110136e00aa67d2b971b9ef4
chamaling <lamin.bangura113@gmail.com>
2026-04-02 11:25:33 -0400

    feat: optimized text size for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Page() {
   return (
     <div className="flex flex-col h-screen items-center justify-center">
       <header>{/* TODO: add icon */}</header>
-      <main className="flex flex-col items-center gap-4">
-        <h1 className="text-5xl font-semibold">Claim your Google Drive storage back</h1>
-        <p className="text-center text-muted-foreground">Drivereview analyzes your Google Drive usage and gives best recommendations for optimizing your storage.</p>
+      <main className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-4xl md:text-5xl font-semibold ">Claim your Google Drive storage back</h1>
+        <p className="text-center text-muted-foreground mb-8">Drivereview analyzes your Google Drive usage and gives best recommendations for optimizing your storage.</p>
         <GoogleDriveButton />
       </main>
       <footer className="mt-auto text-sm text-muted-foreground">
