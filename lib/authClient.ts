53f2d79e14258330a0506bfebc7f0918c6b55727
chamaling <lamin.bangura113@gmail.com>
2026-04-03 16:52:47 -0400

    feat: added authentication middleware client for actions

---
+++ lib/authClient.ts
@@ -0,0 +1,26 @@
+import { actionClient } from "./actionClient"
+import { getSession, type SessionID } from "./sessionManager"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+import makeGoogleAuth from "./makeGoogleAuth"
+export const authClient = actionClient.use(async ({ next }) => {
+  const cookieStore = await cookies()
+  const sessionId = cookieStore.get("session")?.value as SessionID
+
+  if (!sessionId) {
+    redirect("/")
+  }
+
+  const session = getSession(sessionId)
+  if (!session) {
+    redirect("/")
+  }
+
+  const oauthClient = makeGoogleAuth(session.tokens)
+
+  return next({
+    ctx: {
+      oauthClient,
+    },
+  })
+})
