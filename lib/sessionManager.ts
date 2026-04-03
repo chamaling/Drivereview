0522ae3ebad4960b3945ec9a8c6cb8a1d42d0e61
chamaling <lamin.bangura113@gmail.com>
2026-04-03 15:59:30 -0400

    feat: added in memory session manager for access tokens

---
+++ lib/sessionManager.ts
@@ -0,0 +1,42 @@
+/*
+* DEV NOTE:
+* This is a basic in-memory session manager. I am using this for simplicity as it is
+* a personal project and not designed for scale. It would instead be best practice to use
+* a more robust solution like Redis in production.
+*/
+export type SessionID = string & {__brand: "SessionID"}
+
+
+type Session = {
+    accessToken: string;
+    expiresAt: number;
+}
+
+type SessionStore = Map<SessionID, Session>;
+
+const sessions: SessionStore = new Map();
+
+export function addSession(newSessionId: SessionID, accessToken: string): void {
+        const expiresAt = Date.now() + 60 * 60 * 1000; // 1 hour
+
+        sessions.set(newSessionId, {
+            accessToken,
+            expiresAt
+        });
+}
+
+function removeSession(sessionId: SessionID): void {
+    sessions.delete(sessionId);
+}
+
+export function getSession(sessionId: SessionID): Session | undefined {
+    const session = sessions.get(sessionId);
+    if (!session) {
+        return undefined;
+    }
+    if (Date.now() >= session.expiresAt) {
+        removeSession(sessionId);
+        return undefined;
+    }
+    return session;
+}
