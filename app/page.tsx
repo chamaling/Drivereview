8181319242da1edc1f6eb33838a2c3353c574c66
chamaling <lamin.bangura113@gmail.com>
2026-04-02 20:15:12 -0400

    feat: made hero page responsive

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Page() {
   return (
     <div className="flex flex-col h-screen items-center justify-center">
       <header>{/* TODO: add icon */}</header>
-      <main className="flex flex-col items-center gap-4 flex-1 justify-center">
-        <h1 className="text-4xl md:text-5xl font-semibold text-center">Claim your Google Drive storage back</h1>
+      <main className="flex flex-col gap-4 flex-1 justify-center items-center mt-8 md:mt-0 px-2">
+        <h1 className= "w-full max-w-4xl px-4 text-2xl sm:text-4xl font-semibold text-center">Claim your Google Drive storage back</h1>
         <p className="text-center text-muted-foreground mb-8">Drivereview analyzes your Google Drive usage and gives best recommendations for optimizing your storage.</p>
         <GoogleDriveButton />
-      <ol className="mt-16 text-lg text-muted-foreground grid grid-cols-[repeat(3,minmax(150,1fr))] items-center gap-x-16 *:flex *:items-center *:relative *:flex-col *:gap-y-4 *:text-center">
+      <ol className="mt-16 text-lg text-muted-foreground grid grid-cols-1 md:grid-cols-[repeat(3,minmax(150px,1fr))] items-center gap-x-16 *:flex *:items-center *:relative *:flex-col *:gap-y-4 *:text-center">
         <li className="mr-2">Connect to Drive <HeroArrowConnectorIcon />
       <HeroImage
       src="/placeholder.svg"
