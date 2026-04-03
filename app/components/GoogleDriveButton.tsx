188e10bc6d15802144a183a34ec105509ba75dd3
chamaling <lamin.bangura113@gmail.com>
2026-04-03 01:21:05 -0400

    feat: added error labeling for google drive button

--- app/components/GoogleDriveButton.tsx
+++ app/components/GoogleDriveButton.tsx
@@ -4,10 +4,10 @@ import { clientConfig } from '@/lib/config/client';
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useRef } from "react";
-
+import { useState } from "react";
 export default function GoogleDriveButton() {
   const codeClient = useRef<google.accounts.oauth2.CodeClient | null>(null);
-
+  const [error, setError] = useState<string | null>(null);
   function handleGoogleScriptLoad() {
         codeClient.current = google.accounts.oauth2.initCodeClient({
         client_id: clientConfig.GOOGLE_CLIENT_ID,
@@ -17,9 +17,11 @@ export default function GoogleDriveButton() {
         /* TODO: Handle the authorization response */
         if (response.error) {
             console.error("Error during Google OAuth:", response.error);
+            setError("Failed to connect to Google Drive. Please try again.");
             return;
         }
 
+          console.log("Authorization code received:", response.code);
         }
     });
 
@@ -27,14 +29,14 @@ export default function GoogleDriveButton() {
   }
 
   function handleGoogleScriptError() {
-    console.error("Failed to load the Google accounts script.")
+    setError("Failed to load Google, please refresh the page and try again.");
   }
 
   function handleButtonClick() {
     if (codeClient.current) {
       codeClient.current.requestCode();
     } else {
-      console.error("Google OAuth client is not initialized.");
+      setError("Failed to initialize Google OAuth client.");
     }
   }
   return <>
@@ -47,5 +49,6 @@ export default function GoogleDriveButton() {
     <Button className="py-5" onClick={handleButtonClick}>
       <Image src="/google-drive-icon.png" alt="Google Drive Icon" width={1280/60} height={1144/60} className="mr-2" />
       Connect to Google Drive</Button>
+      {error && <p className="text-destructive text-xs text-center">{error}</p>}
   </>
 }
