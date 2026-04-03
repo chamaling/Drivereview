55e1e2209a28366be500cad80d06927e19c80cd1
chamaling <lamin.bangura113@gmail.com>
2026-04-02 20:43:30 -0400

    feat: arrows now point downward responsively

--- app/components/HeroArrowConnectorIcon.tsx
+++ app/components/HeroArrowConnectorIcon.tsx
@@ -1,7 +1,7 @@
 export default function HeroArrowConnectorIcon() {
-    return <div className="absolute top-1/2 -translate-y-1/2 left-full translate-x-full">
+    return <div className="absolute translate-y-1/2 top-full md:top-1/2 md:-translate-y-1/2 md:left-full md:translate-x-full">
 
     
-    <span className="text-2xl block leading-none h-auto" aria-hidden>➲</span>
+    <span className="text-2xl block leading-none h-auto rotate-90 md:rotate-0 md:translate-y-0" aria-hidden>➲</span>
     </div>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Page() {
   return (
     <div className="flex flex-col h-screen items-center justify-center">
       <header>{/* TODO: add icon */}</header>
-      <main className="flex flex-col gap-4 flex-1 justify-center items-center mt-8 md:mt-0 px-2">
+      <main className="flex flex-col gap-4 flex-1 justify-center items-center mt-16 md:mt-0 px-2">
         <h1 className= "w-full max-w-4xl px-4 text-2xl sm:text-4xl font-semibold text-center">Claim your Google Drive storage back</h1>
         <p className="text-center text-muted-foreground mb-8">Drivereview analyzes your Google Drive usage and gives best recommendations for optimizing your storage.</p>
         <GoogleDriveButton />
-      <ol className="mt-16 text-lg text-muted-foreground grid grid-cols-1 md:grid-cols-[repeat(3,minmax(150px,1fr))] items-center gap-x-16 *:flex *:items-center *:relative *:flex-col *:gap-y-4 *:text-center">
+      <ol className=" mt-8 md:mt-16 text-lg text-muted-foreground grid grid-cols-1 md:grid-cols-[repeat(3,minmax(150px,1fr))] items-center gap-x-16 gap-y-8 *:flex *:items-center *:relative *:flex-col *:gap-y-4 *:text-center">
         <li className="mr-2">Connect to Drive <HeroArrowConnectorIcon />
       <HeroImage
       src="/placeholder.svg"
