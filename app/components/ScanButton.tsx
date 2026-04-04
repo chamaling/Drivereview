f0fa397f4325f11527696c6f29ef41e768bf876e
chamaling <lamin.bangura113@gmail.com>
2026-04-04 14:56:10 -0400

    feat: implemented scan button to be shared by empty and filled data states

---
+++ app/components/ScanButton.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/components/ui/button"
+
+export default function ScanButton() {
+  return <Button size="lg">Scan drive</Button>
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,29 +1,29 @@
 import { EmptyIcon } from "@phosphor-icons/react/dist/ssr"
 import {
   Empty,
-  EmptyContent,
   EmptyDescription,
   EmptyHeader,
   EmptyMedia,
   EmptyTitle,
 } from "@/components/ui/empty"
-import { Button } from "@/components/ui/button"
-
+import ScanButton from "../components/ScanButton"
 export default function Page() {
   return (
-    <Empty className="min-h-screen">
-      <EmptyHeader>
-        <EmptyMedia variant="icon" className="size-12 bg-transparent">
-          <EmptyIcon className="size-full!" weight="fill" />
-        </EmptyMedia>
-        <EmptyTitle className="text-lg">No drive data</EmptyTitle>
-        <EmptyDescription className="text-xs">
-          Click the button below to scan your drive!
-        </EmptyDescription>
-      </EmptyHeader>
-      <EmptyContent>
-        <Button size="lg">Scan drive</Button>
-      </EmptyContent>
-    </Empty>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div className="my-auto flex flex-col items-center justify-center">
+        <Empty>
+          <EmptyHeader>
+            <EmptyMedia variant="icon" className="size-12 bg-transparent">
+              <EmptyIcon className="size-full!" weight="fill" />
+            </EmptyMedia>
+            <EmptyTitle className="text-lg">No drive data</EmptyTitle>
+            <EmptyDescription className="text-xs">
+              Click the button below to scan your drive!
+            </EmptyDescription>
+          </EmptyHeader>
+        </Empty>
+        <ScanButton />
+      </div>
+    </div>
   )
 }
