de953a85ea85736ca2056af7b46e1ca6b2e0b0ff
chamaling <lamin.bangura113@gmail.com>
2026-04-04 15:13:46 -0400

    feat: added dialog base for scanbutton

--- app/components/ScanButton.tsx
+++ app/components/ScanButton.tsx
@@ -1,5 +1,29 @@
+"use client"
 import { Button } from "@/components/ui/button"
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { useState } from "react"
 export default function ScanButton() {
-  return <Button size="lg">Scan drive</Button>
+  const [open, setOpen] = useState(true)
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>Scan drive</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  )
 }
