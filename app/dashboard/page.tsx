ddad315fc4d40e32e48069c43e33ec1e87cd035e
chamaling <lamin.bangura113@gmail.com>
2026-04-04 13:59:05 -0400

    feat: added default empty icon for dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,27 @@
+import { EmptyIcon } from "@phosphor-icons/react/dist/ssr"
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty"
+import { Button } from "@/components/ui/button"
 
 export default function Page() {
-    return <p>You&apos;ve successfully connected to your Google Drive!</p>;
+  return (
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <EmptyIcon />
+        </EmptyMedia>
+        <EmptyTitle>No data</EmptyTitle>
+        <EmptyDescription>No data found</EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <Button>Add data</Button>
+      </EmptyContent>
+    </Empty>
+  )
 }
