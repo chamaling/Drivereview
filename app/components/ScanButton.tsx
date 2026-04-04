109eaf8288428fab3148f25cc127e0d7fb9848c3
chamaling <lamin.bangura113@gmail.com>
2026-04-04 15:17:43 -0400

    feat: restructured dialog with another base

--- app/components/ScanButton.tsx
+++ app/components/ScanButton.tsx
@@ -2,27 +2,43 @@
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { Field, FieldGroup } from "@/components/ui/field"
+import { Label } from "@/components/ui/label"
+
 import { useState } from "react"
 export default function ScanButton() {
   const [open, setOpen] = useState(true)
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger>Scan drive</DialogTrigger>
-      <DialogContent>
+      <DialogTrigger render={<Button variant="outline">Open Dialog</Button>} />
+      <DialogContent className="sm:max-w-sm">
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle>Edit profile</DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            Make changes to your profile here. Click save when you&apos;re done.
           </DialogDescription>
         </DialogHeader>
+        <FieldGroup>
+          <Field>
+            <Label htmlFor="name-1">Name</Label>
+          </Field>
+          <Field>
+            <Label htmlFor="username-1">Username</Label>
+          </Field>
+        </FieldGroup>
+        <DialogFooter>
+          <DialogClose render={<Button variant="outline">Cancel</Button>} />
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   )
