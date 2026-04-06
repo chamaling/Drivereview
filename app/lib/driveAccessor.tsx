afdf520f10bf7d5997303a5ac471e0c8c3f3ce55
chamaling <lamin.bangura113@gmail.com>
2026-04-05 22:47:19 -0400

    feat: updated client filters schema with new filters type

--- app/lib/driveAccessor.tsx
+++ app/lib/driveAccessor.tsx
@@ -1,18 +1,12 @@
 import "server-only"
 import * as z from "zod"
 
-import { fileTypeEnum, lastModifiedEnum } from "@/app/schemas/filterSchema"
+import { clientFiltersSchema, ClientFilters } from "../schemas/filterSchema"
 
-export const clientFiltersSchema = z.object({
-  "minimum-file-size": z
-    .number()
-    .min(1)
-    .max(100 * 1024 * 1024 * 1024), // Max 100 GB
-  "file-types": z
-    .array(fileTypeEnum)
-    .max(6)
-    .refine((arr) => new Set(arr).size === arr.length, {
-      message: "File types must be unique.",
-    }),
-  "last-modified": lastModifiedEnum,
-})
+export function validateClientFilters(filters: unknown): ClientFilters {
+  const result = clientFiltersSchema.safeParse(filters)
+  if (!result.success) {
+    throw new Error("Invalid filters: " + result.error.message)
+  }
+  return result.data
+}
--- app/schemas/filterSchema.ts
+++ app/schemas/filterSchema.ts
@@ -15,4 +15,22 @@ export const lastModifiedEnum = z.enum([
   "All files",
 ])
 
-export type FileType = z.infer<typeof fileTypeEnum>
+export const clientFiltersSchema = z.object({
+  "minimum-file-size": z
+    .number("Minimum file size must be a number.")
+    .min(1, "Minimum file size must be at least 1 byte.")
+    .max(
+      100 * 1024 * 1024 * 1024,
+      "Maximum file size must be less than 100 GB."
+    ), // Max 100 GB
+  "file-types": z
+    .array(fileTypeEnum, "File types must be an array of valid file types.")
+    .min(1, "File types must contain at least 1 type.")
+    .max(6, "File types cannot contain more than 6 types.")
+    .refine((arr) => new Set(arr).size === arr.length, {
+      message: "File types must be unique.",
+    }),
+  "last-modified": lastModifiedEnum,
+})
+
+export type ClientFilters = z.infer<typeof clientFiltersSchema>
