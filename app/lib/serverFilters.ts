e993b09e21900e118a432b7102e32f891cb862ba
chamaling <lamin.bangura113@gmail.com>
2026-04-14 00:05:05 -0400

    feat: added types and made server filters server only

--- app/lib/serverFilters.ts
+++ app/lib/serverFilters.ts
@@ -1,5 +1,12 @@
+import "server-only"
+import { ClientFilters } from "@/app/schemas/filterSchema"
+
 export const serverFilters = {
   shared: false,
   contentRestrictions: "none",
   capabilities: [],
 }
+
+export type ServerFilters = typeof serverFilters
+
+export type GlobalFilters = typeof serverFilters & ClientFilters
