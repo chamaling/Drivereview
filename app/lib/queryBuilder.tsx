9f0505e547468ab9cdf70c5dda706d781d21ada0
chamaling <lamin.bangura113@gmail.com>
2026-04-17 15:06:11 -0400

    feat: added last modified string to drive query

--- app/lib/queryBuilder.tsx
+++ app/lib/queryBuilder.tsx
@@ -1,6 +1,32 @@
 import { GlobalFilters } from "./serverFilters"
 
+function convertLastModifiedStringtoUTCTimestamp(
+  lastModified: GlobalFilters["last-modified"]
+): string {
+  const now = new Date()
+  if (lastModified === "Last 30 days") {
+    now.setDate(now.getDate() - 30)
+  } else if (lastModified === "Last 90 days") {
+    now.setDate(now.getDate() - 90)
+  } else if (lastModified === "Last year") {
+    now.setFullYear(now.getFullYear() - 1)
+  } else if (lastModified === "Last 2 years") {
+    now.setFullYear(now.getFullYear() - 2)
+  } else {
+    return ""
+  }
+  return now.toUTCString()
+}
+
 export function buildDriveQuery(globalFilters: GlobalFilters) {
   const currQuery = `'me' in owners`
-  return currQuery
+  const queries: string[] = [currQuery]
+
+  const lastModifiedString = convertLastModifiedStringtoUTCTimestamp(
+    globalFilters["last-modified"]
+  )
+  if (lastModifiedString) {
+    queries.push(`modifiedTime > '${lastModifiedString}'`)
+  }
+  return queries.join(" and ")
 }
