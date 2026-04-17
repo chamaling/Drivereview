ee1cb0964bfcfd1e35d7fb5c73e0f3a633cb04cd
chamaling <lamin.bangura113@gmail.com>
2026-04-17 00:27:25 -0400

    feat: made scan drive action have its own error handling

--- app/actions/scanDriveAction.tsx
+++ app/actions/scanDriveAction.tsx
@@ -2,11 +2,20 @@
 import { authClient } from "@/lib/authClient"
 import { getDriveFiles } from "@/app/lib/driveAccessor"
 import { clientFiltersSchema } from "../schemas/filterSchema"
-
+import ActionError from "@/classes/ActionError"
 export const scanDriveAction = authClient
   .inputSchema(clientFiltersSchema)
-  .action(async ({ parsedInput: clientFilters, ctx }) => {
-    const oauthClient = ctx.oauthClient
+  .action(
+    async ({ parsedInput: clientFilters, ctx }) => {
+      try {
+        const oauthClient = ctx.oauthClient
 
-    await getDriveFiles(oauthClient, clientFilters)
-  })
+        await getDriveFiles(oauthClient, clientFilters)
+      } catch {
+        throw new ActionError("Failed to scan drive. Please try again.")
+      }
+    },
+    {
+      throwServerError: true,
+    }
+  )
