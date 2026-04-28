35639d19e18dceaeb98d3d35f40cf55ddc295d1c
chamaling <lamin.bangura113@gmail.com>
2026-04-28 16:04:56 -0400

    feat: added general size processor

---
+++ app/lib/processors/general-service/sizeProcessor.tsx
@@ -0,0 +1,36 @@
+import "server-only"
+
+import Processor from "../processor"
+import { drive_v3 } from "googleapis"
+
+class SizeProcessor extends Processor {
+  constructor(weight: number) {
+    super(weight)
+  }
+
+  process(file: drive_v3.Schema$File): number {
+    if (!file.size) {
+      return 0
+    }
+    const sizeInMB = parseInt(file.size) / (1024 * 1024)
+
+    // Larger files are more efficient to delete, so they should have a higher score
+    if (sizeInMB < 1) {
+      return 0
+    } else if (sizeInMB < 100) {
+      return 0.5
+    } else {
+      return 1
+    }
+  }
+}
+
+/*
+The file size as a general metric is robust in
+identifying if a file is worth to delete,
+as users are more likely to delete large files than small files,
+so it should have a higher weight
+*/
+const sizeProcessor = new SizeProcessor(0.5)
+
+export default sizeProcessor
