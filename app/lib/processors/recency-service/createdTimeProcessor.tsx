fb2c849b5496ca33022b6fecc12541a02df3212f
chamaling <lamin.bangura113@gmail.com>
2026-04-28 13:11:56 -0400

    feat: added created time processor for recency service

---
+++ app/lib/processors/recency-service/createdTimeProcessor.tsx
@@ -0,0 +1,29 @@
+import "server-only"
+
+import Processor from "../processor"
+import { drive_v3 } from "googleapis"
+
+class CreatedTimeProcessor extends Processor {
+  constructor(weight: number) {
+    super(weight)
+  }
+
+  process(file: drive_v3.Schema$File): number {
+    if (!file.createdTime) {
+      return 0
+    }
+    const createdTime = new Date(file.createdTime).getTime()
+    const currentTime = Date.now()
+    const ageInDays = (currentTime - createdTime) / (1000 * 60 * 60 * 24)
+
+    if (ageInDays < 1) {
+      return 1
+    }
+
+    return 1 / ageInDays
+  }
+}
+
+const createdTimeProcessor = new CreatedTimeProcessor(0.33)
+
+export default createdTimeProcessor
