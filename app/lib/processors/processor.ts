736772ff114101fc75b8c3837bcb1692153a0eec
chamaling <lamin.bangura113@gmail.com>
2026-04-28 10:31:03 -0400

    refactor: processing now returns number instead of promise

--- app/lib/processors/processor.ts
+++ app/lib/processors/processor.ts
@@ -3,5 +3,5 @@ import { drive_v3 } from "googleapis"
 
 export default interface Processor {
   weight: number
-  process: (file: drive_v3.Schema$File) => Promise<number>
+  process: (file: drive_v3.Schema$File) => number
 }
--- app/lib/rating-services/service.ts
+++ app/lib/rating-services/service.ts
@@ -5,5 +5,5 @@ import { drive_v3 } from "googleapis"
 export default interface RatingService {
   weight: number
   processors: Proccessor[]
-  aggregate: (file: drive_v3.Schema$File) => Promise<number>
+  aggregate: (file: drive_v3.Schema$File) => number
 }
