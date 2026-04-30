bfe2ce9071ba6edb32bcce06ca83647ef979b2db
chamaling <lamin.bangura113@gmail.com>
2026-04-29 23:54:26 -0400

    feat: styles can now process name strings

--- app/lib/processors/authority-service/nameProcessor/slices/fileSlice.ts
+++ app/lib/processors/authority-service/nameProcessor/slices/fileSlice.ts
@@ -1,26 +1,25 @@
-const fileSlice = {
+const fileobject = {
   // images with these keywords are typically default names for photos
-  // so likely less important, but not completely uninmportant
-  IMG_: 0.5,
-  Screenshot: 0.5,
-  DSC_: 0.5,
-  DCIM: 0.5,
+  // so likely less important, but not completely unimportant
+  img_: 0.5,
+  screenshot: 0.5,
+  dsc_: 0.5,
+  dcim: 0.5,
 
   // videos with these keywords are typically default names for videos
-  // so likely less important, but not completely uninmportant
-  VID_: 0.5,
-  MOV_: 0.5,
-  Screen_Recording: 0.5,
-  "Screen Recording": 0.5,
+  // so likely less important, but not completely unimportant
+  vid_: 0.5,
+  mov_: 0.5,
+  screen_recording: 0.5,
+  "screen recording": 0.5,
 
   // files with these keywords are typically default names for documents, presentations, or spreadsheets
   // since they're untitled, that is a stronger signal that they may be less important
   // so give it a lower score than other default names
-  "Untitled document": 0.25,
-  "Untitled spreadsheet": 0.25,
-  "Untitled presentation": 0.25,
-  // files created like these are duplicates, so usually less important
-  "Copy of": 0.5,
-}
+  "untitled document": 0.25,
+  "untitled spreadsheet": 0.25,
+  "untitled presentation": 0.25,
 
-export default fileSlice
+  // files created like these are duplicates, so usually less important
+  "copy of": 0.5,
+}
--- app/lib/processors/authority-service/nameProcessor/slices/slice.ts
+++ app/lib/processors/authority-service/nameProcessor/slices/slice.ts
@@ -2,7 +2,8 @@ import keywordSettings from "../keywordSettings"
 
 class Slice {
   private regObject: RegExp
-
+  private defaultWeight: number = 1
+  private keywordMap: Record<string, number> | null = null
   constructor(keywordArray: string[], defaultWeight: number)
   constructor(keywordMap: Record<string, number>)
 
@@ -18,6 +19,7 @@ class Slice {
       }
 
       this.regObject = new RegExp(`(${keywordObject.join("|")})`, "i")
+      this.defaultWeight = defaultWeight
     } else {
       if (Array.isArray(keywordObject)) {
         throw new Error(
@@ -26,6 +28,19 @@ class Slice {
       }
       const keywords = Object.keys(keywordObject)
       this.regObject = new RegExp(`(${keywords.join("|")})`, "i")
+      this.defaultWeight = 1
+      this.keywordMap = keywordObject
     }
+  }
+
+  process(name: string): number {
+    const match = name.match(this.regObject)
+    if (!match) {
+      return 1
+    }
+    if (this.keywordMap) {
+      return this.keywordMap[match[0].toLowerCase()] || this.defaultWeight
+    }
+    return this.defaultWeight
   }
 }
