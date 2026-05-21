2ae39d67075b8e2bc26bf70091418e10bb584ec2
chamaling <lamin.bangura113@gmail.com>
2026-05-21 00:03:34 -0400

    refactor: reduced weight of recency service

--- app/lib/rating-services/authorityService.ts
+++ app/lib/rating-services/authorityService.ts
@@ -5,7 +5,7 @@ import starredProcessor from "../processors/authority-service/starredProcessor"
 import mimeTypeProcessor from "../processors/authority-service/mimeTypeProcessor"
 import nameProcessor from "../processors/authority-service/nameProcessor/nameProcessor"
 
-const authorityService = new RatingService(0.5, [
+const authorityService = new RatingService(0.65, [
   createdTimeProcessor,
   modifiedTimeProcessor,
   starredProcessor,
--- app/lib/rating-services/recencyService.ts
+++ app/lib/rating-services/recencyService.ts
@@ -2,7 +2,7 @@ import RatingService from "./service"
 import createdTimeProcessor from "../processors/recency-service/createdTimeProcessor"
 import modifiedTimeProcessor from "../processors/recency-service/modifiedTimeProcessor"
 
-const recencyService = new RatingService(0.25, [
+const recencyService = new RatingService(0.1, [
   createdTimeProcessor,
   modifiedTimeProcessor,
 ])
